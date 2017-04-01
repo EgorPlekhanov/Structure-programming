6ed89a8f48714790e6b54ad58d14c5947bc4ebb8
EgorPlekhanov <eg0r.plekhan0v@yandex.ru>
2017-04-01 13:05:13 +0500

    Add files via upload

---
+++ 1.1 E.py
@@ -0,0 +1,6 @@
+import math as m
+x=float(input("Введите x: "))
+t=float(input("Введите t: "))
+K=float
+K=7*pow(t,2)+3*m.sin(pow(x,3))+9.2
+print("K=",K)
---
+++ 1.1 M.py
@@ -0,0 +1,6 @@
+import math as m
+y=int(input("Введите y: "))
+t=int(input("Введите t: "))
+R=float
+R=((m.sin(2*t+1)**2)+0.3)/(m.log(t+y))
+print("R=",R)
